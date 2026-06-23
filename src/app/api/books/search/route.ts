c8830aeab47ecdbc09e54cc8d4973ce92047a58a
hsw824 <ghdtjddn0824@naver.com>
2026-06-23 14:44:01 +0900

    카카오책 api 추가

---
+++ src/app/api/books/search/route.ts
@@ -0,0 +1,26 @@
+import { KakaoBookResponese } from "@/models/bookTypes";
+
+export async function GET(request: Request) {
+  const query = new URL(request.url).searchParams.get("query");
+
+  const response = await fetch(
+    `https://dapi.kakao.com/v3/search/book?query=${encodeURIComponent(query!)}`,
+    { headers: { Authorization: `KakaoAK ${process.env.KAKAO_REST_API_KEY}` } },
+  );
+
+  const data: KakaoBookResponese = await response.json();
+
+  const book = data.documents.map((book) => ({
+    authors: book.authors.join(","),
+    contents: book.contents,
+    datetime: book.datetime,
+    publisher: book.publisher,
+    thumbnail: book.thumbnail,
+    title: book.title,
+    translators: book.translators.join(","),
+    url: book.url,
+    isbn: book.isbn.split(" ")[1],
+  }));
+
+  return Response.json(book);
+}
