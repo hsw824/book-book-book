0c16217b9e9fb3cce4578649823fabe9701948e4
hsw824 <ghdtjddn0824@naver.com>
2026-07-21 15:56:42 +0900

    feat: 라우트 정리 기본 홈화면 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import { Form } from '@/components/Form';
+import Link from 'next/link';
 
 export default function Home() {
-  return <Form />;
+  return (
+    <div>
+      <h1 className="mb-8 text-5xl">홈화면</h1>
+      <Link className="border" href="/records/new">
+        새 글 쓰기
+      </Link>
+    </div>
+  );
 }
