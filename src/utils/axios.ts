7062dc63b52b17c065d8df2be2e1194e5c50e75a
hsw824 <ghdtjddn0824@naver.com>
2026-07-24 16:38:00 +0900

    feat: http post 함수 추가

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,14 +1,15 @@
-import Axios, { AxiosRequestConfig } from "axios";
+import Axios, { AxiosRequestConfig } from 'axios';
 
 const axios = Axios.create();
 
 export const http = {
-  get: async function get<Response = unknown>(
-    url: string,
-    option: AxiosRequestConfig = {},
-  ) {
+  get: async function get<Response = unknown>(url: string, option: AxiosRequestConfig = {}) {
     const result = await axios.get<Response>(url, option);
+    return result.data;
+  },
 
+  post: async function post<Request = unknown, Response = unknown>(url: string, data: Request) {
+    const result = await axios.post<Response>(url, data);
     return result.data;
   },
 };
