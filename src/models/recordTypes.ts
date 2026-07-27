6d86fe10ba6ca9ddd76b58dd223931ae06d615c1
hsw824 <ghdtjddn0824@naver.com>
2026-07-27 17:49:08 +0900

    refactor: records(홈 화면 리스트)와 record(상세 페이지 정보) 구분

--- src/models/recordTypes.ts
+++ src/models/recordTypes.ts
@@ -6,11 +6,39 @@ interface ShortedBookType {
   publisher: string;
 }
 
-export interface RecordType {
+export interface DetailBookType extends ShortedBookType {
+  isbn: string;
+  publishedYear: string;
+}
+
+export interface SimpleRecordType {
   id: string;
   rating: number;
   category: string;
   isEbook: boolean;
   finishedAt: string;
   book: ShortedBookType;
+}
+
+export interface QuoteType {
+  id: string;
+  recordId: string;
+  page: number;
+  text: string;
+  order: number;
+}
+
+export interface RecordType {
+  id: string;
+  userId: string;
+  bookId: string;
+  category: string;
+  rating: number;
+  review: string;
+  isEbook: boolean;
+  finishedAt: string;
+  createdAt: string;
+  updatedAt: string;
+  book: DetailBookType;
+  quotes: QuoteType[];
 }
--- src/services/record.ts
+++ src/services/record.ts
@@ -1,9 +1,13 @@
 import { BookForm } from '@/components/Form';
-import { RecordType } from '@/models/recordTypes';
+import { RecordType, SimpleRecordType } from '@/models/recordTypes';
 import { http } from '@/utils/axios';
 
 export const fetchRecords = () => {
-  return http.get<RecordType[]>('/api/records');
+  return http.get<SimpleRecordType[]>('/api/records');
+};
+
+export const fetchRecord = (id: string) => {
+  return http.get<RecordType>(`/api/records/${id}`);
 };
 
 export const postRecord = async (data: BookForm) => {
