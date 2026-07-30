f7eb6500c8dea60b8f078b6c2db5b22c4067e479
hsw824 <ghdtjddn0824@naver.com>
2026-07-30 17:32:14 +0900

    feat: react query devtools 추가

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 
@@ -21,6 +22,7 @@ export function Providers({ children }: { children: ReactNode }) {
     <QueryClientProvider client={queryClient}>
       {children}
       <Toaster position="top-right" toastOptions={{ duration: 2000, error: { duration: 3000 } }} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
