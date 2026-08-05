302137597aa22aca0c609656738f42a5b991fe82
hsw824 <ghdtjddn0824@naver.com>
2026-08-05 16:12:20 +0900

    refactor: query가 없을 경우 api 에러 처리 추가

--- src/app/api/books/search/route.ts
+++ src/app/api/books/search/route.ts
@@ -1,23 +1,26 @@
-import { KakaoBookResponese } from "@/models/bookTypes";
+import { KakaoBookResponese } from '@/models/bookTypes';
 
 export async function GET(request: Request) {
-  const query = new URL(request.url).searchParams.get("query");
+  const query = new URL(request.url).searchParams.get('query');
 
-  const response = await fetch(
-    `https://dapi.kakao.com/v3/search/book?query=${encodeURIComponent(query!)}`,
-    { headers: { Authorization: `KakaoAK ${process.env.KAKAO_REST_API_KEY}` } },
-  );
+  if (!query) {
+    return Response.json({ error: 'query는 필수입니다.' }, { status: 400 });
+  }
+
+  const response = await fetch(`https://dapi.kakao.com/v3/search/book?query=${encodeURIComponent(query)}`, {
+    headers: { Authorization: `KakaoAK ${process.env.KAKAO_REST_API_KEY}` },
+  });
 
   const data: KakaoBookResponese = await response.json();
 
-  const books = data.documents.map((book) => ({
-    authors: book.authors.join(","),
+  const books = data.documents.map(book => ({
+    authors: book.authors.join(','),
     contents: book.contents,
     datetime: book.datetime,
     publisher: book.publisher,
     thumbnail: book.thumbnail,
     title: book.title,
-    translators: book.translators.join(","),
+    translators: book.translators.join(','),
     url: book.url,
     isbn: book.isbn,
   }));
