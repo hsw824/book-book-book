95aaaeaae1556e5146a69f89ae9cf1bbb05672e0
hsw824 <ghdtjddn0824@naver.com>
2026-08-06 14:01:06 +0900

    refactor: Star Icon 분리

--- src/components/Form/RatingField.tsx
+++ src/components/Form/RatingField.tsx
@@ -4,6 +4,7 @@ import { isNull } from 'es-toolkit';
 import { Rating, RATINGS } from '@/constants/rate';
 import { BookRecordForm } from '@/models/recordTypes';
 import { useState } from 'react';
+import { Star } from '../icons/Star';
 
 const RATING_TEXTS = ['별로예요', '아쉬워요', '보통이에요', '좋아요', '최고예요'];
 
@@ -30,9 +31,7 @@ export function RatingField({ control }: { control: Control<BookRecordForm> }) {
                   onMouseEnter={() => setHoverRating(rate)}
                   onMouseLeave={() => setHoverRating(null)}
                 >
-                  <svg width="26" height="26" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
-                  </svg>
+                  <Star width={26} height={26} />
                 </span>
               ))}
               <span className="ml-3.5 text-[13px] text-gray-600">
