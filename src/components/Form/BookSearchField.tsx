7447d99ab855aaf685e766d0101b48f8d60ed8ee
hsw824 <ghdtjddn0824@naver.com>
2026-08-06 14:11:12 +0900

    refactor: SelectedBookContent 추가

--- src/components/Form/BookSearchField.tsx
+++ src/components/Form/BookSearchField.tsx
@@ -3,6 +3,7 @@ import { Control, Controller, useFormState } from 'react-hook-form';
 import { FormField } from './FormField';
 import Image from 'next/image';
 import { AutoCompleteInput } from '../AutoCompleteInput';
+import { Book } from '@/models/bookTypes';
 
 export function BookSearchField({ control }: { control: Control<BookRecordForm> }) {
   const { errors } = useFormState({ control, name: 'bookInfo' });
@@ -14,23 +15,7 @@ export function BookSearchField({ control }: { control: Control<BookRecordForm> 
         name="bookInfo"
         render={({ field: { onChange, value } }) => {
           return value ? (
-            <div className="flex items-start gap-3.5 rounded-xl bg-zinc-200 px-4 py-3.5">
-              <Image src={value.thumbnail} alt="책 사진" width={46} height={64} className="rounded-[3px]" />
-              <div className="items-between flex basis-[82%] flex-col">
-                <span className="text-[15px] font-semibold">{value.title}</span>
-                <span className="text-[12px]">{value.authors}</span>
-                <span className="text-[12px]">
-                  {value.publisher} · {new Date(value.datetime).getFullYear()}
-                </span>
-              </div>
-              <button
-                type="button"
-                className="cursor-pointer rounded-2xl bg-white px-3 py-1.5 text-[12px] text-gray-600"
-                onClick={() => onChange(null)}
-              >
-                변경
-              </button>
-            </div>
+            <SelectedBookContent value={value} onChange={onChange} />
           ) : (
             <AutoCompleteInput onChange={onChange} />
           );
@@ -40,5 +25,27 @@ export function BookSearchField({ control }: { control: Control<BookRecordForm> 
 
       {errors.bookInfo && <span className="text-sm text-red-600">{errors.bookInfo.message}</span>}
     </FormField>
+  );
+}
+
+function SelectedBookContent({ value, onChange }: { value: Book; onChange: (...event: null[]) => void }) {
+  return (
+    <div className="flex items-start gap-3.5 rounded-xl bg-zinc-200 px-4 py-3.5">
+      <Image src={value.thumbnail} alt="책 사진" width={46} height={64} className="rounded-[3px]" />
+      <div className="items-between flex basis-[82%] flex-col">
+        <span className="text-[15px] font-semibold">{value.title}</span>
+        <span className="text-[12px]">{value.authors}</span>
+        <span className="text-[12px]">
+          {value.publisher} · {new Date(value.datetime).getFullYear()}
+        </span>
+      </div>
+      <button
+        type="button"
+        className="cursor-pointer rounded-2xl bg-white px-3 py-1.5 text-[12px] text-gray-600"
+        onClick={() => onChange(null)}
+      >
+        변경
+      </button>
+    </div>
   );
 }
