ff4c7e4fc1647d079bc11bb63d7b5fbf0227e630
hsw824 <ghdtjddn0824@naver.com>
2026-08-10 22:35:41 +0900

    refactor: datetime, thumbnail타입 publishedYear, coverUrl타입으로 통일

--- src/app/api/books/search/route.ts
+++ src/app/api/books/search/route.ts
@@ -1,4 +1,4 @@
-import { KakaoBookResponese } from '@/models/bookTypes';
+import { KakaoBookResponse, BookSummary } from '@/models/bookTypes';
 
 export async function GET(request: Request) {
   const query = new URL(request.url).searchParams.get('query');
@@ -11,9 +11,9 @@ export async function GET(request: Request) {
     headers: { Authorization: `KakaoAK ${process.env.KAKAO_REST_API_KEY}` },
   });
 
-  const data: KakaoBookResponese = await response.json();
+  const data: KakaoBookResponse = await response.json();
 
-  const books = data.documents.map(book => ({
+  const books: BookSummary[] = data.documents.map(book => ({
     authors: book.authors.join(','),
     publishedYear: new Date(book.datetime).getFullYear(),
     publisher: book.publisher,
