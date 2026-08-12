8880976a0e99ba07a389e16942d179797fbcf04e
hsw824 <ghdtjddn0824@naver.com>
2026-08-12 22:22:22 +0900

    feat: auth토큰 없을 경우 페이지 리디렉션 추가

---
+++ src/proxy.ts
@@ -0,0 +1,14 @@
+import { auth } from '@/auth';
+
+export default auth(req => {
+  const isLoggedIn = !!req.auth;
+  const isAuthPage = req.nextUrl.pathname === '/login';
+
+  if (!isLoggedIn && !isAuthPage) {
+    return Response.redirect(new URL('/login', req.nextUrl));
+  }
+});
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
